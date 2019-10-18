a06f0f239691edc36c6afb6a166459ff81a45696
ArthropodKing <56170060+ArthropodKing@users.noreply.github.com>
2019-10-18 10:19:13 -0700

    Update HelloWorldApp.java

--- Assignment1/HelloWorldApp.java
+++ Assignment1/HelloWorldApp.java
@@ -1,2 +1,17 @@
-// Student name:
+// Callum:
 // This is where your first assignment goes!
+/**
+* The HelloWorldApp class implements an application that simply
+* displays "Hello World!" to the standard output.
+*/
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Best animal is Spider");
+ System.out.println("Best Pokemon is Klefki");
+ System.out.println("10+10 Be Yo Mom");
+ System.out.println(10+10);
+ System.out.println("Who killed JFK was Me and the Boys");
+ System.out.println("Denmark is Not a real country It Is Actually three Countries In A Coat");
+ System.out.println("Submission date Is Now");
+    }
+}
